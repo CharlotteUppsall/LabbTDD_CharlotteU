4da5235e6d6e8cd3ec73c6aa13880b260a7bcbf0
CharlotteU <charlotte.uppsall@iths.se>
2022-10-22 22:46:57 +0200

    RED

--- src/test/java/TestEmployeeSystem.java
+++ src/test/java/TestEmployeeSystem.java
@@ -1,3 +1,9 @@
+import org.junit.jupiter.api.Test;
+
 public class TestEmployeeSystem {
 
+    @Test
+    public void showEmployeeSystemList(){
+        EmployeeSystem testObject1 = new EmployeeSystem();
+    }
 }
