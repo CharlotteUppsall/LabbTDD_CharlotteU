36e053a9e1bab1194f3351d82d79d72241a8f3f6
CharlotteU <charlotte.uppsall@iths.se>
2022-10-22 16:23:31 +0200

    RED

--- src/test/java/TestEmployeeInfo.java
+++ src/test/java/TestEmployeeInfo.java
@@ -1,6 +1,9 @@
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
 
 public class TestEmployeeInfo {
 
     @Test
+    public void checkEmployeeFirstName(){
+        EmployeeInfo testObject = new EmployeeInfo();
     }
+}
