cd963cf610a82c4ff6b8bebdf61b6176a3099e98
CharlotteU <charlotte.uppsall@iths.se>
2022-10-22 16:53:30 +0200

    GREEN

--- src/test/java/TestEmployeeInfo.java
+++ src/test/java/TestEmployeeInfo.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestEmployeeInfo {
@@ -5,5 +6,11 @@ public class TestEmployeeInfo {
     @Test
     public void checkGetEmployeeFirstName(){
         EmployeeInfo testObject = new EmployeeInfo("Charlie");
+
+        String expected = "Charlie";
+        String actual = testObject.getFirstName();
+
+        Assertions.assertEquals(expected, actual);
+
     }
 }
