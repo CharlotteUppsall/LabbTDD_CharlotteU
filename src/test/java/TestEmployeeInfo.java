ad96347fb1411cf5b71f95fc046b0a78693f3224
CharlotteU <charlotte.uppsall@iths.se>
2022-10-22 16:56:51 +0200

    REFACTORED

--- src/test/java/TestEmployeeInfo.java
+++ src/test/java/TestEmployeeInfo.java
@@ -6,12 +6,12 @@ public class TestEmployeeInfo {
     EmployeeInfo testObject = new EmployeeInfo("Charlie");
 
     @Test
-    public void checkGetEmployeeFirstName(){
-
+    public void testGetEmployeeFirstName(){
         String expected = "Charlie";
         String actual = testObject.getFirstName();
 
         Assertions.assertEquals(expected, actual);
-
     }
+
+
 }
